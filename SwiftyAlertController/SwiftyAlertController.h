8d9b318ee1746e66b1227f8c4e2e788703414516
Sourav Chandra <saurav.chandra1992@live.com>
2019-03-18 01:56:55 +0530

    Initial Commit

---
+++ SwiftyAlertController/SwiftyAlertController.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyAlertController.h
+//  SwiftyAlertController
+//
+//  Created by Sourav Chandra on 18/03/19.
+//  Copyright © 2019 Pulse Care. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyAlertController.
+FOUNDATION_EXPORT double SwiftyAlertControllerVersionNumber;
+
+//! Project version string for SwiftyAlertController.
+FOUNDATION_EXPORT const unsigned char SwiftyAlertControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyAlertController/PublicHeader.h>
+
+
